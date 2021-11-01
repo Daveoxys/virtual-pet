c7aa52924c6ea4e470e07abdd600f5f4ea7d9419
Dave Taylor <davidtaylor970@googlemail.com>
2021-11-01 14:52:47 +0000

    virtual-pet progress

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,21 +7,21 @@ describe("constructor", () => {
 
   it("sets the name property", () => {
     const pet = new Pet("Fenrir");
-
     expect(pet.name).toEqual("Fenrir");
   });
 
   it("has a inital age of 0'", () => {
     const pet = new Pet("Fenrir");
-
     expect(pet.age).toEqual(0);
   });
 
   describe("growUp", () => {
     const pet = new Pet("Fenrir");
-
     pet.growUp();
-
     expect(pet.age).toEqual(1);
+  });
+
+  it("Pet initial hunger equals 0", () => {
+    expect(pet.hunger).toEqual(0);
   });
 });
